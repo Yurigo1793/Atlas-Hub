e953c65569b8efabc3218478894c2a8ca5f914e3
YuriGo <yurigo1793@gmail.com>
2026-04-08 09:12:47 -0300

    Remove ChatGPT filename icon lookup and use app.ico only

--- main.cpp
+++ main.cpp
@@ -1,15 +1,54 @@
 #include <QApplication>
+#include <QChar>
+#include <QDir>
+#include <QFileInfo>
 #include <QIcon>
+
+#ifdef _WIN32
+#include <ShObjIdl_core.h>
+#endif
 
 #include "core/AppController.h"
 #include "utils/Logger.h"
 
+namespace
+{
+QIcon resolveAppIcon()
+{
+    const QString bundledSvg = QStringLiteral(":/branding/logo.svg");
+    const QStringList searchRoots = {
+        QCoreApplication::applicationDirPath(),
+        QDir::current().filePath(QStringLiteral("assets")),
+        QDir::currentPath()
+    };
+
+    for (const QString &root : searchRoots) {
+        const QString expectedIco = QDir(root).filePath(QStringLiteral("app.ico"));
+        if (QFileInfo::exists(expectedIco)) {
+            Logger::instance().info(QStringLiteral("Using application icon: %1").arg(expectedIco));
+            return QIcon(expectedIco);
+        }
+    }
+
+    Logger::instance().warning(QStringLiteral("No app.ico file found. Falling back to bundled SVG icon."));
+    return QIcon(bundledSvg);
+}
+}
+
 int main(int argc, char *argv[])
 {
+#ifdef _WIN32
+    const HRESULT appIdResult = SetCurrentProcessExplicitAppUserModelID(L"AtlasHub.App");
+    if (FAILED(appIdResult)) {
+        Logger::instance().warning(QStringLiteral("Failed to set AppUserModelID. HRESULT=0x%1")
+                                       .arg(static_cast<qulonglong>(static_cast<unsigned long>(appIdResult)), 8, 16, QChar(u'0')));
+    }
+#endif
+
     QApplication app(argc, argv);
     QApplication::setApplicationName("AtlasHub");
     QApplication::setOrganizationName("AtlasHub");
-    app.setWindowIcon(QIcon(QStringLiteral(":/branding/logo.svg")));
+    app.setWindowIcon(resolveAppIcon());
 
     Logger::instance().info("Application starting");
 
